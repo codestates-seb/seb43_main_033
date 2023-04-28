1481e6c4b64e80f2d355f6d4354e7faa75bdd444
RapiL <sjw3090gmail.com>
2023-04-28 19:03:15 +0900

    init

---
+++ main/src/main/java/main/main/exception/ExceptionCode.java
@@ -0,0 +1,25 @@
+package main.main.exception;
+
+import lombok.Getter;
+
+public enum ExceptionCode {
+    MEMBER_NOT_FOUND(404, "Member not found"),
+    MEMBER_EMAIL_EXISTS(409, "email exists"),
+    MEMBER_NAME_EXISTS(409, "name exists"),
+    ANSWER_NOT_FOUND(404, "Answer not found"),
+    QUESTION_NOT_FOUND(404, "Question not found"),
+    TAG_NOT_FOUND(404, "Tag not found"),
+    ONLY_AUTHOR(403, "You don't have permission"),
+    UNAUTHORIZED(401,  "Unauthorized");
+
+    @Getter
+    private int status;
+    @Getter
+    private String message;
+
+    ExceptionCode(int code, String message) {
+        this.status = code;
+        this.message = message;
+    }
+
+}
