ce01221bcf957afaccac24f8d95d7f9bd11ee251
DDunDDang <thdwns0331@gmail.com>
2023-05-02 21:09:27 +0900

    Feat: SalaryStatement CR

--- main/src/main/java/main/main/salarystatement/controller/SalaryStatementController.java
+++ main/src/main/java/main/main/salarystatement/controller/SalaryStatementController.java
@@ -1,15 +1,32 @@
 package main.main.salarystatement.controller;
 
 import lombok.RequiredArgsConstructor;
+import main.main.salarystatement.dto.SalaryStatementDto;
+import main.main.salarystatement.entity.SalaryStatement;
 import main.main.salarystatement.mapper.SalaryStatementMapper;
 import main.main.salarystatement.service.SalaryStatementService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/salarystatement")
+@RequestMapping("/salarystatements")
 @RequiredArgsConstructor
 public class SalaryStatementController {
     private final SalaryStatementService salaryStatementService;
     private final SalaryStatementMapper salaryStatementMapper;
+
+    @PostMapping
+    public ResponseEntity postSalaryStatement(@RequestBody SalaryStatementDto.Post requestBody) {
+        salaryStatementService.createSalaryStatement(salaryStatementMapper.postToSalaryStatement(requestBody));
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{salarystatement-id}")
+    public ResponseEntity getSalaryStatement(@PathVariable("salarystatement-id") long salaryStatementId) {
+        SalaryStatement salaryStatement = salaryStatementService.findSalaryStatement(salaryStatementId);
+
+        return new ResponseEntity<>(salaryStatementMapper.salaryStatementToResponse(salaryStatement), HttpStatus.OK);
+    }
 }
--- main/src/main/java/main/main/salarystatement/dto/SalaryStatementDto.java
+++ main/src/main/java/main/main/salarystatement/dto/SalaryStatementDto.java
@@ -1,4 +1,38 @@
 package main.main.salarystatement.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
 public class SalaryStatementDto {
+    @Getter
+    @Setter
+    public static class Post {
+        private long companyId;
+        private long memberBankId;
+        private long memberId;
+        private int year;
+        private int month;
+    }
+
+    @Getter
+    @Builder
+    public static class Response {
+        private long id;
+        private long companyId;
+        private String companyName;
+        private long memberId;
+        private String memberName;
+        private int year;
+        private int month;
+        private double hourlyWage;
+        private double basePay;
+        private double overtimePay;
+        private int overtimePayBasis;
+        private double nightWorkAllowance;
+        private int nightWorkAllowanceBasis;
+        private double holidayWorkAllowance;
+        private int holidayWorkAllowanceBasis;
+        private double salary;
+    }
 }
