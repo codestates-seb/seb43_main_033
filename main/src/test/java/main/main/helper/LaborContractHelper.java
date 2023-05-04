300e04736341d94788be0c8cdb4460d01c3ff3a4
DDunDDang <thdwns0331@gmail.com>
2023-05-04 14:40:03 +0900

    Test: Docs

--- main/src/test/java/main/main/helper/LaborContractHelper.java
+++ main/src/test/java/main/main/helper/LaborContractHelper.java
@@ -12,7 +12,7 @@ public interface LaborContractHelper extends ControllerHelper {
     String LABORCONTRACT_RESOURCE_ID = "/{laborcontract-id}";
     String LABORCONTRACT_RESOURCE_URI = LABORCONTRACT_DEFAULT_URL + LABORCONTRACT_RESOURCE_ID;
 
-    default List<ParameterDescriptor> getMemberRequestPathParameterDescriptor() {
+    default List<ParameterDescriptor> getRequestPathParameterDescriptor() {
         return Arrays.asList(parameterWithName("laborcontract-id").description("근로계약서 식별 번호"));
     }
 }
