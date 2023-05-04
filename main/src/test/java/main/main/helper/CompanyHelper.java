1e44715adfce057d0ce63fa9aaa81883f55a5435
swj8560 <swj5514@gmail.com>
2023-05-04 20:06:48 +0900

    Test: CompanyController

--- main/src/test/java/main/main/helper/CompanyHelper.java
+++ main/src/test/java/main/main/helper/CompanyHelper.java
@@ -1,4 +1,18 @@
 package main.main.helper;
 
-public interface CompanyHelper {
+import org.springframework.restdocs.request.ParameterDescriptor;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+
+public interface CompanyHelper extends ControllerHelper {
+    String COMPANY_DEFAULT_URL = "/companies";
+    String COMPANY_RESOURCE_ID = "/{company-id}";
+    String COMPANY_RESOURCE_URI = COMPANY_DEFAULT_URL + COMPANY_RESOURCE_ID;
+
+    default List<ParameterDescriptor> getCompanyRequestPathParameterDescriptor() {
+        return Arrays.asList(parameterWithName("company-id").description("회사 식별 번호"));
+    }
 }
