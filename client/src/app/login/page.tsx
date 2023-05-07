c775af3cb872e6378bc50b59cfc3a70b0214e87f
lyssoi <114637463+lyssoi@users.noreply.github.com>
2023-05-08 01:55:11 +0900

    로그인 페이지 디자인

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -1,79 +1,83 @@
 "use client";
 
-import { useState } from "react";
-import axios from "axios";
+import { ChangeEvent, Dispatch, SetStateAction, useState } from "react";
+import axios, { AxiosResponseHeaders } from "axios";
 
 export default function login() {
   const [isLogin, setIsLogin] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const handleChange = (e, set) => {
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement>,
+    set: Dispatch<SetStateAction<string>>
+  ) => {
     set(e.target.value);
   };
   const handleClick = () => {
     loginAxios();
   };
   const loginAxios = () => {
-    // axios.defaults.withCredentials = true;
     axios
-      .post(
-        "https://c49c-61-254-8-200.ngrok-free.app/login",
-        {
-          email,
-          password,
-        }
-        // {
-        //   headers: {
-        //     "Content-Type": "application/json",
-        //     // "Access-Control-Allow-Origin": "*",
-        //     // "Access-Control-Allow-Headers":
-        //     //   "Origin, X-Requested-With, Content-Type, Accept",
-        //     "Access-Control-Expose-Headers": "Authorization",
-        //     // "ngrok-skip-browser-warning": "69420",
-        //   },
-        // }
-      )
+      .post("https://c49c-61-254-8-200.ngrok-free.app/login", {
+        email,
+        password,
+      })
       .then((response) => {
-        // 백엔드가 헤더에 담아서 보내준 토큰을 가져옴
         const token = response.headers.authorization;
-        // 로컬에 토큰을 저장하는 함수 매개변수로 토큰을 받음
-        const saveToken = (token) => {
+        const saveToken = (token: string) => {
           localStorage.setItem("token", token);
         };
-
-        // 멤버id(유저id)도 헤더에 담긴 값을 가져옴
         const memberid = response.headers.memberid;
-        // 로컬에 저장하는 함수 매개변수로 id를 받음
-        const saveMemberId = (memberid) => {
+        const saveMemberId = (memberid: string) => {
           localStorage.setItem("memberid", memberid);
         };
-
-        //응답 성공코드가 올때 실행
         if (response.status === 200 || response.status === 201) {
-          /*  로그인 성공시
-          => 로그인 상태 변경, 토큰과 아이디 로컬에 저장, 디스패치로 Nav와 Footer보이게 true설정, 네비게이트로 페이지 이동 */
           setIsLogin(false);
           saveToken(token);
           saveMemberId(memberid);
         }
       })
       .catch((err) => {
-        /*응답이 안될때 로그인 상태 변경 / 콘솔 오류코드*/
         console.log(err);
       });
   };
   return (
-    <main key="login" className="w-full h-full flex justify-center">
-      <div className="flex justify-center">
+    <main
+      key="login"
+      className="w-screen flex items-center justify-center bg-gray-50 p-48 flex-col"
+    >
+      <div className="flex flex-col justify-center p-12 rounded-lg shadow-lg bg-white w-[366px]">
+        <div className="flex flex-row justify-center mb-10">
+          <h1 className="font-semibold text-green-500">WORKER LOGIN</h1>
+        </div>
+        <label htmlFor="email" className="font-semibold text-gray-700">
+          email
+        </label>
         <input
-          placeholder="email"
+          className="w-[270px] outline-none border rounded-sm px-3 py-1 focus:border-green-500 mb-2"
+          id="email"
           onChange={(e) => handleChange(e, setEmail)}
         ></input>
+        <label htmlFor="email" className="font-semibold text-gray-700">
+          password
+        </label>
         <input
-          placeholder="password"
+          className="w-[270px] outline-none border rounded-sm px-3 py-1 focus:border-green-500 mb-2"
+          id="password"
           onChange={(e) => handleChange(e, setPassword)}
         ></input>
-        <button onClick={handleClick}>submit</button>
+        <button
+          className="mt-10 bg-green-400 rounded-md py-2 text-white hover:bg-green-300"
+          onClick={handleClick}
+        >
+          submit
+        </button>
+      </div>
+      <div className="flex mt-3">
+        <p>계정이 없으신가요?</p>
+        <a className="ml-3 text-blue-400" href="/signup">
+          회원가입
+        </a>
       </div>
     </main>
   );
