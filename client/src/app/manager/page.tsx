5b343754b0e80efb6395139eb662b178bc62be76
SEB_FE_43_유호균 <74657430+ghrbsaod@users.noreply.github.com>
2023-05-08 14:55:58 +0900

    머지용 푸시

--- client/src/app/manager/page.tsx
+++ client/src/app/manager/page.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link"
 import Navi from "../Navi"
-import Information from "../components/HomeComponents/TopInformation"
+import TopInformation from "../components/HomeComponents/TopInformation"
+import MiddleInformation from "../components/HomeComponents/MiddleInformation"
+import BottomInformation from "../components/HomeComponents/BottomInformation"
 
 
 export default function ManagerHome() {
@@ -9,36 +11,9 @@ export default function ManagerHome() {
     <Navi />
       {/* <Modal /> */}
       <section className="flex-1 flex flex-col px-8 py-4 bg-stone-50">
-        <Information />
-        <article className="flex flex-row flex-wrap min-h-32 bg-white p-3 mb-5 rounded drop-shadow">
-          <section className="flex-1 flex-col p-4">
-            <div className="flex justify-between bg-stone-200 p-2 mb-3 rounded">
-              <span className="">저번 달 급여내역</span>
-            </div>
-            <div className="flex flex-col p-2 bg-stone-100 min-h-32 rounded">
-              <span>안녕</span>
-            </div>
-          </section>
-          <section className="flex-1 flex-col p-4">
-            <div className="flex justify-between bg-stone-200 p-2 mb-3 rounded">
-              <span className="">이번 달 급여내역</span>
-            </div>
-            <div className="flex flex-col p-2 bg-stone-100 min-h-32 rounded">
-              <span>안녕</span>
-            </div>
-          </section>
-        </article>
-        <article className="flex flex-col flex-wrap min-h-32 bg-white p-6 mb-5 rounded drop-shadow">
-          <div className="flex justify-between items-center bg-stone-200 p-2 mb-3 rounded">
-            <span className="mx-4 font-semibold text-2xl">
-              이번 달 특이사항
-            </span>
-            <span className="mx-4 bg-stone-50 px-2 py-1 rounded-sm font-semibold drop-shadow">
-              수정
-            </span>
-          </div>
-          <section className="flex flex-colmin-h-32">안녕</section>
-        </article>
+        <TopInformation />
+        <MiddleInformation />
+        <BottomInformation />
       </section>
     </>
   );
