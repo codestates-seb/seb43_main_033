f6a4111f25fab6f367b6dfb75c878c6e3f133c4e
swj8560 <swj5514@gmail.com>
2023-05-09 01:06:06 +0900

    Feat: CompanyMember

--- main/src/main/java/main/main/companymember/mapper/CompanyMemberMapper.java
+++ main/src/main/java/main/main/companymember/mapper/CompanyMemberMapper.java
@@ -1,7 +1,73 @@
 package main.main.companymember.mapper;
 
+import main.main.company.entity.Company;
+import main.main.companymember.dto.CompanyMemberDto;
+import main.main.companymember.entity.CompanyMember;
+import main.main.member.entity.Member;
 import org.mapstruct.Mapper;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface CompanyMemberMapper {
+
+    default CompanyMember companyMemberPostToCompanyMember(CompanyMemberDto.Post requestBody) {
+        CompanyMember companyMember = new CompanyMember();
+        Company company = new Company();
+        Member member  = new Member();
+
+        company.setCompanyId(requestBody.getCompanyId());
+        member.setMemberId(requestBody.getMemberId());
+
+        companyMember.setCompany(company);
+        companyMember.setMember(member);
+        companyMember.setGrade(requestBody.getGrade());
+        companyMember.setTeam(requestBody.getTeam());
+
+        return companyMember;
+    }
+
+    default CompanyMember companyMemberPacthToCompanyMember(CompanyMemberDto.Patch requestBody) {
+        CompanyMember companyMember = new CompanyMember();
+        Company company = new Company();
+        Member member = new Member();
+
+        company.setCompanyId(requestBody.getCompanyId());
+        member.setMemberId(requestBody.getMemberId());
+
+        companyMember.setCompany(company);
+        companyMember.setMember(member);
+        companyMember.setCompanyMemberId(requestBody.getCompanyMemberId());
+        companyMember.setGrade(requestBody.getGrade());
+        companyMember.setTeam(requestBody.getTeam());
+
+        return companyMember;
+    }
+
+    default CompanyMemberDto.Response companyMemberToCompanyMemberResponse(CompanyMember companyMember) {
+        return CompanyMemberDto.Response.builder()
+                .companyMemberId(companyMember.getCompanyMemberId())
+                .companyId(companyMember.getCompany().getCompanyId())
+                .memberId(companyMember.getMember().getMemberId())
+                .grade(companyMember.getGrade())
+                .team(companyMember.getTeam())
+                .build();
+    }
+    default CompanyMemberDto.ResponseForList companyMemberToCompanyMemberResponseForList(CompanyMember companyMember) {
+        return CompanyMemberDto.ResponseForList.builder()
+                .companyMemberId(companyMember.getCompanyMemberId())
+                .companyId(companyMember.getCompany().getCompanyId())
+                .memberId(companyMember.getMember().getMemberId())
+                .grade(companyMember.getGrade())
+                .team(companyMember.getTeam())
+                .build();
+    }
+
+
+    default List<CompanyMemberDto.ResponseForList> companyMembersToCompanyMembersResponse(List<CompanyMember> companyMembers) {
+        return  companyMembers.stream()
+                .map(companyMember -> companyMemberToCompanyMemberResponseForList(companyMember))
+                .collect(Collectors.toList());
+    }
 }
--- main/src/main/java/main/main/companymember/service/CompanyMemberService.java
+++ main/src/main/java/main/main/companymember/service/CompanyMemberService.java
@@ -1,11 +1,69 @@
 package main.main.companymember.service;
 
 import lombok.RequiredArgsConstructor;
+import main.main.company.entity.Company;
+import main.main.company.service.CompanyService;
+import main.main.companymember.entity.CompanyMember;
 import main.main.companymember.repository.CompanyMemberRepository;
+import main.main.exception.BusinessLogicException;
+import main.main.exception.ExceptionCode;
+import main.main.member.entity.Member;
+import main.main.member.service.MemberService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class CompanyMemberService {
+
     private final CompanyMemberRepository companyMemberRepository;
+    private final CompanyService companyService;
+    private final MemberService memberService;
+
+    public CompanyMember createCompanyMember(CompanyMember companyMember) {
+
+        Company company = companyService.findCompany(companyMember.getCompany().getCompanyId());
+        Member member = memberService.findMember(companyMember.getMember().getMemberId());
+
+        companyMember.setCompany(company);
+        companyMember.setMember(member);
+
+        return companyMemberRepository.save(companyMember);
+    }
+
+    public CompanyMember findCompanyMember(long companyMemberId) {
+        return findVerifiedCompanyMember(companyMemberId);
+    }
+
+    public CompanyMember findVerifiedCompanyMember(long companyMemberId) {
+        Optional<CompanyMember> optionalCompanyMember = companyMemberRepository.findById(companyMemberId);
+        CompanyMember findedCompanyMember = optionalCompanyMember.orElseThrow(()->new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
+        return findedCompanyMember;
+    }
+
+    public Page<CompanyMember> findCompanyMembers(int page, int size) {
+        return companyMemberRepository.findAll(PageRequest.of(page, size, Sort.by("companyMemberId").descending()));
+    }
+
+    public void deleteCompanyMember(long companyMemberId) {
+        CompanyMember findedCompanyMember = findVerifiedCompanyMember(companyMemberId);
+        companyMemberRepository.delete(findedCompanyMember);
+    }
+
+
+    public CompanyMember updateCompanyMember(CompanyMember companyMember) {
+        CompanyMember findedCompanyMember = findVerifiedCompanyMember(companyMember.getCompanyMemberId());
+
+        Optional.ofNullable(companyMember.getGrade())
+                .ifPresent(grade -> findedCompanyMember.setGrade(grade));
+        Optional.ofNullable(companyMember.getTeam())
+                .ifPresent(team -> findedCompanyMember.setTeam(team));
+
+        return companyMemberRepository.save(findedCompanyMember);
+    }
+
 }
