3a71fd208f86fcecd1cccd088b72b7ec76d1e30f
swj8560 <swj5514@gmail.com>
2023-05-10 02:22:03 +0900

    Feat: totalSalaryOfCompany

--- main/src/main/java/main/main/company/entity/Company.java
+++ main/src/main/java/main/main/company/entity/Company.java
@@ -3,8 +3,10 @@ package main.main.company.entity;
 import lombok.Getter;
 import lombok.Setter;
 import main.main.companymember.entity.CompanyMember;
+import main.main.salarystatement.entity.SalaryStatement;
 
 import javax.persistence.*;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,10 +24,15 @@ public class Company {
     private String businessNumber;
     private String address;
     private String information;
+    private BigDecimal totalSalaryOfCompany;
 
 
 
     @OneToMany(mappedBy = "company", cascade = CascadeType.PERSIST)
     private List<CompanyMember> companyMembers = new ArrayList<>();
     public void addCompanyMember(CompanyMember companyMember) { this.companyMembers.add(companyMember); }
+
+    @OneToMany(mappedBy = "company")
+    private List<SalaryStatement> salaryStatements = new ArrayList<>();
+    public void addSalaryStatement(SalaryStatement salaryStatement) { this.salaryStatements.add(salaryStatement); }
 }
