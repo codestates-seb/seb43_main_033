9f254d51abbb54acf2fe921bfb9936e82cd07dd1
DDunDDang <thdwns0331@gmail.com>
2023-05-09 12:14:19 +0900

    Fix: add findByMemberAndCompany()

--- main/src/main/java/main/main/companymember/repository/CompanyMemberRepository.java
+++ main/src/main/java/main/main/companymember/repository/CompanyMemberRepository.java
@@ -1,7 +1,10 @@
 package main.main.companymember.repository;
 
+import main.main.company.entity.Company;
 import main.main.companymember.entity.CompanyMember;
+import main.main.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CompanyMemberRepository extends JpaRepository<CompanyMember, Long> {
+    CompanyMember findByMemberAndCompany(Member member, Company company);
 }
