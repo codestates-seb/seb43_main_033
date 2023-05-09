71e9fc4a371778b3ba246cf33cec5b3db2b096b7
swj8560 <swj5514@gmail.com>
2023-05-09 12:09:37 +0900

    Init: CompanyMemberControllerTest

---
+++ main/src/test/java/main/main/helper/CompanyMemberHelper.java
@@ -0,0 +1,18 @@
+package main.main.helper;
+
+import org.springframework.restdocs.request.ParameterDescriptor;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+
+public interface CompanyMemberHelper extends ControllerHelper {
+    String COMPANYMEMBER_DEFAULT_URL = "/companymembers";
+    String COMPANYMEMBER_RESOURCE_ID = "/{companymember-id}";
+    String COMPANYMEMBER_RESOURCE_URI = COMPANYMEMBER_DEFAULT_URL + COMPANYMEMBER_RESOURCE_ID;
+
+    default List<ParameterDescriptor> getCompanyMemberRequestPathParameterDescriptor() {
+        return Arrays.asList(parameterWithName("companymember-id").description("회사 등록 직원"));
+    }
+}
