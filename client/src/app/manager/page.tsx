5787bfce898ce28c360a262bb9e56bf376a0acc7
ysycoding <syy4242@gmail.com>
2023-05-11 14:11:50 +0900

    merge2

--- client/src/app/manager/page.tsx
+++ client/src/app/manager/page.tsx
@@ -1,54 +1,18 @@
-//import { useState } from 'react';
-"use client";
-import { useState } from "react";
-import GreenTop from "../components/PaystubPage/GreenTop";
-import StaffSelect from "../components/StaffSelect";
-import StaffSelectModal from "../components/StaffSelectModal";
-import PaystubPreview from "../components/PaystubPage/PaystubPreview";
-import WorkingStatus from "../components/PaystubPage/WorkingStatus";
+import Link from "next/link";
 import Navi from "../components/managerNavi";
-import AccountAdd from "@/app/components/PaystubPage/AccountAdd";
+import TopInformation from "../components/ManagerHome/TopInformation";
+import MiddleInformation from "../components/ManagerHome/MiddleInformation";
+import BottomInformation from "../components/ManagerHome/BottomInformation";
 
-const Paystub = () => {
-  const [showModal, setShowModal] = useState(false);
-  // const [selectedStaff, setSelectedStaff] = useState('');
-
-  //  const [isChecked, setIsChecked] = useState<boolean>(false);
+export default function ManagerHome() {
   return (
     <>
       <Navi />
-      <div className="">
-        <GreenTop>직원선택</GreenTop>
-        <div className="bg-white p-3 m-5 flex justify-between">
-          <StaffSelect>홍길동</StaffSelect>
-          <button
-            className="text-sm font-bold hover:bg-gray-300 px-2"
-            onClick={() => setShowModal(true)}
-          >
-            직원선택하기
-          </button>
-        </div>
-        {showModal && (
-          <StaffSelectModal onClose={() => setShowModal(false)}>
-            <StaffSelect>ddd</StaffSelect>
-            <StaffSelect>ddd</StaffSelect>
-            <StaffSelect>ddd</StaffSelect>
-            <StaffSelect>ddd</StaffSelect>
-            <StaffSelect>ddd</StaffSelect>
-            <StaffSelect>ddd</StaffSelect>
-            <StaffSelect>ddd</StaffSelect>
-            <StaffSelect>ddd</StaffSelect>
-          </StaffSelectModal>
-        )}
-        <GreenTop>계좌번호</GreenTop>
-        <AccountAdd />
-        <GreenTop>근태</GreenTop>
-        <WorkingStatus></WorkingStatus>
-        <GreenTop>지급내역</GreenTop>
-        <PaystubPreview></PaystubPreview>
-      </div>
+      <section className="flex flex-col px-8 py-4 w-screen bg-stone-50">
+        <TopInformation />
+        <MiddleInformation />
+        <BottomInformation />
+      </section>
     </>
   );
-};
-
-export default Paystub;
+}
