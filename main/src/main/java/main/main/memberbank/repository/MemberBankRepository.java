ccf273fb6eb953f445332195fe49175767520042
swj8560 <swj5514@gmail.com>
2023-05-24 11:58:18 +0900

    Fix: getMemberBanksByMemberId 추가

--- main/src/main/java/main/main/memberbank/repository/MemberBankRepository.java
+++ main/src/main/java/main/main/memberbank/repository/MemberBankRepository.java
@@ -3,8 +3,12 @@ package main.main.memberbank.repository;
 import main.main.memberbank.entity.MemberBank;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberBankRepository extends JpaRepository<MemberBank, Long> {
     Optional<MemberBank> findByAccountNumber(String accountNumber);
+
+    List<MemberBank> findByMemberMemberId(long memberId);
+
 }
