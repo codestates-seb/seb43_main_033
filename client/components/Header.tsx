cb6ef55526a4984e40b7d94692df18fb60a58171
김영택 <kooduck168@gmail.com>
2023-05-25 15:07:45 +0900

    Pull병합

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -5,12 +5,13 @@ export default function Header() {
   return (
     <header className="bg-white h-28 drop-shadow-lg z-10 w-screen top-0 fixed ">
       <div className="bg-emerald-400 h-8 flex justify-end align-middle">
-        
-          <Link href="/login"><button className="px-5 ">로그인</button></Link>
-        
-        
-          <Link href="/signup"><button className="px-5">회원가입</button></Link>
-        
+        <Link href="/login">
+          <button className="px-5 ">로그인</button>
+        </Link>
+
+        <Link href="/signup">
+          <button className="px-5">회원가입</button>
+        </Link>
       </div>
       <div className="flex justify-evenly align-middle h-20">
         <link
@@ -18,12 +19,15 @@ export default function Header() {
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"
         />
         <Logo />
-        
-          <Link href="/manager"><button className="text-2xl">관리자 메뉴</button></Link>
-        
-        
-          <Link href="/worker"><button className="text-2xl">근로자 메뉴</button></Link>
-        
+
+        <Link href="/manager">
+          <button className="pt-5 text-2xl">관리자 메뉴</button>
+        </Link>
+
+        <Link href="/worker">
+          <button className="pt-5 text-2xl">근로자 메뉴</button>
+        </Link>
+
         <span></span>
         <span></span>
         <span></span>
