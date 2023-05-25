ed1ec87bb658917b8688af5ff8f078ea736bd850
swj8560 <120303851+swj8560@users.noreply.github.com>
2023-05-25 13:03:27 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,5 @@
 PROJECT_ROOT="/home/ubuntu/seb43_main_033/main"
-JAR_NAME="$PROJECT_ROOT/main-0.0.1-SNAPSHOT.jar"
+JAR_NAME="$PROJECT_ROOT/main-gitactions-SNAPSHOT.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
@@ -8,10 +8,10 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 TIME_NOW=$(date +%c)
 
 echo "$TIME_NOW > $JAR_NAME 파일 복사" >> $DEPLOY_LOG
-cp $PROJECT_ROOT/build/libs/*.jar $JAR_NAME
+cp $PROJECT_ROOT/*.jar $JAR_NAME
 
-echo "$TIME_NOW > $JAR_NAME 파일 실행" >> $DEPLOY_LOG
-nohup java -jar $JAR_NAME > $APP_LOG 2> $ERROR_LOG &
+# echo "$TIME_NOW > $JAR_NAME 파일 실행" >> $DEPLOY_LOG
+# nohup java -jar $JAR_NAME > $APP_LOG 2> $ERROR_LOG &
 
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
