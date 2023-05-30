7dcf97c5f8f0c46b332dc0631043466b7c30f9ce
SEB_FE_43_유호균 <74657430+ghrbsaod@users.noreply.github.com>
2023-05-30 14:56:17 +0900

    오어스 수정2

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,10 +1,26 @@
 import MainContents from '../components/Main/MainContents';
 import { Inter } from "next/font/google";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (    
+  const router = useRouter();
+  useEffect(() => {
+    const searchParamsToken = new URLSearchParams(window.location.search);
+    const token = searchParamsToken.get("Authorization");
+    const searchParamsId = new URLSearchParams(window.location.search);
+    const memberid = searchParamsId.get("MemberId");
+    if (token && memberid) {
+      localStorage.setItem("token", token);
+      localStorage.setItem("memberid", memberid);
+      router.push("/");
+      window.location.href = "/";
+    }
+  }, []);
+
+  return (        
     <MainContents />
   )
 }
